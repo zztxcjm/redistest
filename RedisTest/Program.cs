7696a4123c32c8e79fa876cff1f265531bf40b9b
cjm <cjm@handday.com>
2017-04-01 22:15:55 +0800

    添加项目文件。

---
+++ RedisTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var key = "key" + new Random().Next(1, 100);
+
+            ResdisExecutor.Exec((db) =>
+            {
+                db.StringSet(key, key);
+            });
+
+            var val = ResdisExecutor.Exec<string>((db) =>
+            {
+                return db.StringGet(key);
+            });
+
+            Console.WriteLine(val);
+
+        }
+    }
+}
---
+++ RedisTest/ResdisExecutor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StackExchange.Redis;
+
+namespace RedisTest
+{
+
+    public delegate T RedisAction<T>(IDatabase db);
+    public delegate void RedisAction(IDatabase db);
+
+    public class ResdisExecutor
+    {
+
+        private static string GetConfig(string config)
+        {
+            if (!String.IsNullOrEmpty(config))
+            {
+                config = System.Configuration.ConfigurationManager.AppSettings[config];
+            }
+
+            if (String.IsNullOrEmpty(config))
+                config = "127.0.0.1";
+
+            return config;
+
+        }
+
+        public static void Exec(RedisAction act, string config = null)
+        {
+            if (act != null)
+            {
+                using (var client = ConnectionMultiplexer.Connect(GetConfig(config)))
+                {
+                    var db = client.GetDatabase();
+                    if (db != null)
+                    {
+                        act(db);
+                    }
+                }
+            }
+        }
+
+        public static T Exec<T>(RedisAction<T> act, string config = null)
+        {
+            if (act != null)
+            {
+                using (var client = ConnectionMultiplexer.Connect(GetConfig(config)))
+                {
+                    var db = client.GetDatabase();
+                    if (db != null)
+                    {
+                        return act(db);
+                    }
+                }
+            }
+
+            return default(T);
+        }
+
+    }
+
+}
